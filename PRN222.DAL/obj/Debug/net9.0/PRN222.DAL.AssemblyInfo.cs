15ccaa6310e3fcf23507a60dc8cf124be8cc8e50
Dixeliare <daoxuanlong492004@gmail.com>
2025-02-24 00:03:56 +0700

    new

--- PRN222.DAL/obj/Debug/net9.0/PRN222.DAL.AssemblyInfo.cs
+++ PRN222.DAL/obj/Debug/net9.0/PRN222.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRN222.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3b82cc52b911f949b8c425d79b8424f1d1fc8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRN222.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRN222.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PRN222/Program.cs
+++ PRN222/Program.cs
@@ -59,7 +59,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=NewsArticle}/{action=NewsPage}/{id?}")
     .WithStaticAssets();
 
 
